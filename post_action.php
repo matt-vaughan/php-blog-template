1d3f240cd381f64351ec206b3992017732449779
Matthew Vaughan <mvaughan404@gmail.com>
2025-11-14 14:01:19 -0500

    add post page

---
+++ post_action.php
@@ -0,0 +1,13 @@
+<?php
+require_once './database.php';
+
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+$database = new Database();
+
+$database.post($title, $content);
+
+header("Location: index.php");
+exit(); // or die();
+?>
