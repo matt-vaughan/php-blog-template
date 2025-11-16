3b0dedefd58decd925bd33928517beba2007fa7b
Matthew Vaughan <mvaughan404@gmail.com>
2025-11-16 10:52:49 -0500

    remove copyright

--- post.php
+++ post.php
@@ -143,7 +143,7 @@
 
 				<!-- Copyright -->
 					<ul id="copyright">
-						<li>&copy; Untitled.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						<li></li>
 					</ul>
 
 			</div>
